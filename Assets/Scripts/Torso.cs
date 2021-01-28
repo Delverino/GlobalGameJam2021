c3cb6b8e27b3c802885cf92db83f2cf7af78f4fd
Ezra Szanton <ezraszanton@yahoo.com>
2021-01-28 18:23:18 -0500

    Adding some initial test stuff for torso mode

---
+++ Assets/Scripts/Torso.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torso : MonoBehaviour
+{
+    public Rigidbody2D _body;
+    public float _torque;
+
+    public KeyCode _key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_key))
+        {
+            _body.AddTorque(_torque);
+        }
+        else
+        {
+            _body.AddTorque(-_torque);
+        }
+    }
+}
